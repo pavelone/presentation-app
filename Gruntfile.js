39f624c84aee8d5784c24b7f2f54bfc287f5dfa0
niquola <niquola@gmail.com>
2014-11-07 20:36:20 +0400

    stylo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,17 @@
 module.exports = function (grunt) {
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-clean');
 
   var buildDir = (process.env.PREFIX || './dist') + '/';
+
+  [
+  'grunt-contrib-copy',
+  'grunt-contrib-watch',
+  'grunt-contrib-clean',
+  'grunt-contrib-less',
+  'grunt-webpack',
+  'grunt-connect'
+  ].forEach(function(tsk){
+    grunt.loadNpmTasks(tsk);
+  })
 
   grunt.initConfig({
     clean: {
@@ -11,15 +20,60 @@ module.exports = function (grunt) {
     },
     copy: {
      index: {
-       src: 'index.html',
+       src: 'src/index.html',
        dest: buildDir + 'index.html'
      },
      manifest: {
        src: 'fhir.json',
        dest: buildDir + 'fhir.json'
-     }
-    }
+     },
+     fonts: {
+       cwd: "./bower_components/bootstrap/fonts/",
+       expand: true,
+       src: '*',
+       dest: buildDir + 'fonts/'
+     },
+    },
+    webpack: {
+      app: {
+        entry: "./src/coffee/app.coffee",
+        output: {
+          path: buildDir,
+          filename: "app.js",
+          library: "app",
+          libraryTarget: "umd"
+        },
+        module: {
+          loaders: [
+           { test: /\.coffee$/, loader: "coffee-loader" }
+          ]
+        },
+        resolve: { extensions: ["", ".webpack.js", ".web.js", ".js", ".coffee"]}
+      }
+    },
+    less: {
+      development: {
+        options: {
+          paths: ["src/less", "bower_components/bootstrap/less"],
+          cleancss: true,
+          modifyVars: { bgColor: 'white' }
+        },
+        files: { "dist/css/app.css": ['src/less/app.less'] }
+      }
+    },
+    watch: {
+      main: {
+        files: ['src/**/*'],
+        tasks: ['build'],
+        options: {
+          events: ['changed', 'added'],
+          nospawn: true
+        }
+      }
+    },
+   connect: { default: { port: 8080, base: 'dist' } }
   });
 
-  grunt.registerTask('build', ['clean', 'copy']);
+  grunt.registerTask('build', ['clean','webpack','less', 'copy']);
+  grunt.registerTask('server', ['connect']);
 };
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-fhirplace-empty-plugin
+fhirplace example plugin
 ======================
 
-empty plugin
+```sh
+ # install nvm
+ nvm use 0.10
+ npm install
+ bower install
+ `npm bin`/grunt build # build
+ `npm bin`/grunt watch # development
+ `npm bin`/grunt server # dev server
+```
